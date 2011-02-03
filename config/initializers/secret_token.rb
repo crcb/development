667e120ed37e01ba4b8ef8c65aff8ca69fba55d6
Production Site <prod_env@gianfar.dreamhost.com>
2011-02-02 20:36:32 -0800

    is this working?

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crbc::Application.config.secret_token = 'd77bbd65e0f6c170b1af634a880b1d81326427eec28acf69a991678b7b6a39a2618cfe075f819f0a73bacbe08fd4ebfe960fdba05d8cfc1b87ffdba889f0dde0'
